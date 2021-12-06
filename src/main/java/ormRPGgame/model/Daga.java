b456bbfbc737f0752ead767db04f640d00063199
RachelDC00 <67373943+aprentix@users.noreply.github.com>
2021-12-06 09:37:34 +0100

    Update Daga.java

--- src/main/java/ormRPGgame/model/Daga.java
+++ src/main/java/ormRPGgame/model/Daga.java
@@ -5,12 +5,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 // @TODO completar las anotaciones de la clase
+@Entity @Table(name = "daga")
 public class Daga {
     // @TODO completar las anotaciones de todos los atributos
-
+    @Id
+    @Column(name = "nombre_daga")
+    private String nombre;
     public Daga(String nombre){
         // @TODO completar el constructor de la clase.
+        this.nombre=nombre;
     }
 
-    public String getnombre(){ return this.name;}
+    public String getnombre(){ return this.nombre;}
 }
