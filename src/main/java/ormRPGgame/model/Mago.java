4d3cf345c249f1905a88bd75f0f147a6f6934744
RachelDC00 <67373943+aprentix@users.noreply.github.com>
2021-12-06 09:38:53 +0100

    Update Mago.java

--- src/main/java/ormRPGgame/model/Mago.java
+++ src/main/java/ormRPGgame/model/Mago.java
@@ -5,16 +5,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 // @TODO completar las anotaciones de la clase
-
+@Entity
+@Table(name = "mago")
 public class Mago {
     // @TODO completar las anotaciones de todos los atributos
-
-
+    @Id
+    @Column(name = "id")
+    private String name;
+    @Column(name = "daga")
+    Daga daga;
     public Mago(String nombre, Daga daga){
         // @TODO completar el constructor de la clase.
         //  Para ello es necesario un string con el nombre del mago y un objeto de la clase daga
         //  Inicialice el resto de atributos a los valores que considere oportunos
+        this.name=nombre;
+        this.daga=daga;
     }
 
     public String getnombre(){ return this.name;}
 }
+
