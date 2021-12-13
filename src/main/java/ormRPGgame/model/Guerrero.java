266a6a5b710680cf5bfca87adad0c370d6b41892
fatimaakbarova <95278838+fatimaakbarova@users.noreply.github.com>
2021-12-13 19:49:23 +0100

    Update Guerrero.java

--- src/main/java/ormRPGgame/model/Guerrero.java
+++ src/main/java/ormRPGgame/model/Guerrero.java
@@ -20,11 +20,5 @@ public class Guerrero {
 
     }
 
-    public Guerrero(String nombre, Daga daga){
-        // @TODO completar el constructor de la clase.
-        //  Para ello es necesario un string con el nombre del mago y un objeto de la clase daga
-        //  Inicialice el resto de atributos a los valores que considere oportunos
-    }
-
-    public String getnombre(){ return this.nombre;}
+    
 }
