e4afb2314b7df3d02f5b71fce3ca25d1e4ab53d8
RachelDC00 <67373943+aprentix@users.noreply.github.com>
2021-12-20 15:40:03 +0100

    Actualización_del_main

--- HIBERNATE TERMINADO/practica-dragones-y-cabernas-hito3-hibernate-main/src/main/java/Main.java
+++ HIBERNATE TERMINADO/practica-dragones-y-cabernas-hito3-hibernate-main/src/main/java/Main.java
@@ -13,10 +13,16 @@ public class Main {
             Controller controlador= new Controller();
             //@TODO añada las llamadas a las funciones de la clase Controller.java necesarias
             //para la creación de la daga y el mago.
-            Jugador jugador=controlador.createJugador(1,"jugador");
+            Jugador jugador=controlador.createJugador(1,"PLAYER_00");
             Daga daga=controlador.createDaga(1,"Daga de madera");
-            Personaje personaje1=controlador.createPersonaje(jugador,"prueba",daga);
+            Personaje personaje=controlador.createPersonaje(jugador,"VIANA",daga);
             Mago mago=controlador.createMago(jugador,"Stanto",daga);
+            if(jugador!=null && daga!=null && personaje!=null && mago!=null){
+                System.out.println("Se ha creado un mago de nombre: "+mago.getNombrePersonaje());
+                if(daga.getId() == jugador.getId()){
+                    System.out.println("Su daga es: "+daga.getnombre());
+                }
+            }
         } catch(SQLException e) {
             System.err.println("Se ha producido un error en la conexión con la base de datos");
             e.printStackTrace();
